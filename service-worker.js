57bbb9b692dcaed21fb07d136e9cf4a5ff7fe6b9
aarrtthhuurr-bip <arthurdealmeida124@gmail.com>
2026-07-29 12:52:50 -0300

    Reduz interferência do HUD durante as partidas

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_VERSION = "valorant2d-shell-20260729-ingame-remaster";
+const CACHE_VERSION = "valorant2d-shell-20260729-ingame-remaster-compact";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./styles.css?v=20260729-ingame-remaster",
-  "./game.js?v=20260729-ingame-remaster",
+  "./styles.css?v=20260729-ingame-remaster-compact",
+  "./game.js?v=20260729-ingame-remaster-compact",
   "./assets/Favicon/android-chrome-192x192.png",
   "./assets/Favicon/android-chrome-512x512.png",
 ];
